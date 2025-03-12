fd9562ffe1e8a6d47290ff1dc219e10b59dbb463
ssmahim01 <mahimzayn666@gmail.com>
2025-03-12 15:32:06 +0600

    Added redux files and Updated Navbar with Wrapped all inside of provider in main

--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -1,87 +1,117 @@
 import { BiLogInCircle, BiLogOutCircle } from "react-icons/bi";
 import { FaHome } from "react-icons/fa";
-import { MdContacts, MdHealthAndSafety, MdLocalPharmacy, MdMedicalServices, MdMenuBook } from "react-icons/md";
+import {
+  MdContacts,
+  MdHealthAndSafety,
+  MdLocalPharmacy,
+  MdMedicalServices,
+} from "react-icons/md";
 import { FaUserDoctor } from "react-icons/fa6";
 import { Link, NavLink } from "react-router-dom";
 import CareMatrixLogo from "../../assets/Images/logo-care-matrix.webp";
+import { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { closeMenu, toggleMenu } from "@/redux/menuSlice";
 import "./Navbar.css";
 
 const Navbar = () => {
-    const user = null;
+  const dispatch = useDispatch();
+  const menuOpen = useSelector((state) => state.menu.menuOpen);
+  console.log(menuOpen);
+  const dropdownRef = useRef(null);
+  const user = null;
+
+  const handleToggleMenu = () => {
+    dispatch(toggleMenu());
+  };
+
+  const handleClickOutSide = (e) => {
+    if (!e.target.closest(".dropdown")) {
+      dispatch(closeMenu());
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutSide);
+    return () => {
+      document.removeEventListener("click", handleClickOutSide);
+    };
+  }, [dispatch]);
 
   const routes = (
     <>
-      <NavLink
-        className="flex gap-1 items-center"
-        to="/"
-      >
+      <NavLink className="flex gap-1 items-center" to="/">
         <FaHome /> <span className="font-bold">Home</span>
       </NavLink>
-      <NavLink
-        className="flex gap-1 items-center"
-        to="/doctors"
-      >
+      <NavLink className="flex gap-1 items-center" to="/doctors">
         <FaUserDoctor /> <span className="font-bold">Doctors</span>
       </NavLink>
-      <NavLink
-        className="flex gap-1 items-center"
-        to="/services"
-      >
+      <NavLink className="flex gap-1 items-center" to="/services">
         <MdMedicalServices /> <span className="font-bold">Services</span>
       </NavLink>
-      <NavLink
-        className="flex gap-1 items-center"
-        to="/contact-us"
-      >
+      <NavLink className="flex gap-1 items-center" to="/contact-us">
         <MdContacts /> <span className="font-bold">Contact Us</span>
       </NavLink>
-      <NavLink
-        className="flex gap-1 items-center"
-        to="/pharmacy"
-      >
+      <NavLink className="flex gap-1 items-center" to="/pharmacy">
         <MdLocalPharmacy /> <span className="font-bold">Pharmacy</span>
       </NavLink>
-      <NavLink
-        className="flex gap-1 items-center"
-        to="/health-articles"
-      >
+      <NavLink className="flex gap-1 items-center" to="/health-articles">
         <MdHealthAndSafety /> <span className="font-bold">Health Articles</span>
       </NavLink>
     </>
   );
 
   return (
-    <div
-     className="navbar border-b border-[#f3f6f9] fixed z-10 shadow-sm lg:px-12 md:px-6 px-4 bg-[#f3f6f9]"
-    >
+    <div className="navbar border-b border-[#f3f6f9] fixed z-10 shadow-sm lg:px-24 md:px-6 px-4 bg-[#f3f6f9]">
       <div className="navbar-start">
-        <div className="dropdown">
-          <div
+        <div className="dropdown" ref={dropdownRef}>
+          <button
+            onClick={handleToggleMenu}
             tabIndex={0}
             role="button"
-            className="btn border border-neutral-300 btn-ghost lg:hidden mr-2"
+            className="btn btn-outline border border-sky-200 lg:hidden mr-3 shadow-sm"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+            {menuOpen ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />{" "}
+              </svg>
+            )}
+          </button>
+          {menuOpen && (
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100/90 rounded-box z-[10] mt-3 w-60 p-3 shadow gap-4 mr-3"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100/90 rounded-box z-[10] mt-3 w-60 p-3 shadow gap-4 mr-3"
-          >
-            {routes}
-          </ul>
+              {routes}
+            </ul>
+          )}
         </div>
         <div className="flex items-center">
           <img
@@ -101,28 +131,14 @@ const Navbar = () => {
 
       <div className="navbar-end w-full">
         <div className="hidden lg:flex">
-          <ul className="menu menu-horizontal gap-4 mr-3 px-1">
-            {routes}
-          </ul>
+          <ul className="menu menu-horizontal gap-4 mr-3 px-1">{routes}</ul>
         </div>
-
 
         {user ? (
           <div className="flex gap-2 items-center">
-            <figure className="w-12 h-12 avatar avatar-online">
-              <img
-                src={user?.photoURL}
-                alt={user?.displayName}
-                className="w-full h-full border-4 border-indigo-500 rounded-full"
-                referrerPolicy="no-referrer"
-              />
-            </figure>
-
-            <button
-              className="bg-rose-500 cursor-pointer py-2 px-5 text-white hover:bg-rose-700 rounded-md font-bold flex gap-1 items-center"
-            >
-              <BiLogOutCircle className="text-2xl" />
-              <span className="text-base">Log Out</span>
+            <button className="btn btn-outline border-2 px-4 hover:text-white text-rose-500 hover:bg-rose-500 rounded-md font-bold flex gap-1 items-center">
+              <BiLogOutCircle />
+              <span>Log Out</span>
             </button>
           </div>
         ) : (
