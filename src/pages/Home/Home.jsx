01839d0fc37fa22e68856e25f06ac7c7e431002b
ssmahim01 <mahimzayn666@gmail.com>
2025-03-13 11:51:42 +0600

    Added Hero section with images then import it to the Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,51 +7,54 @@ import AboutUs from "@/components/Home/AboutUs";
 import OurDepartments from "@/components/Home/OurDepartments";
 import HowItWorks from "@/components/Home/HowItWorks";
 import Doctors from "./Doctors";
+import Hero from "@/components/Home/Hero";
 
 const Home = () => {
   return (
-    <section className="lg:w-4/5 w-11/12 mx-auto">
-      {/* Banner */}
-
-      {/* About us */}
-    <div className="py-16">
-    <AboutUs/>
-    </div>
-            {/* About us */}
-
-      {/* Our Services */}
-
-      {/* Our Departments */}
-      <div className="md:pb-24 pb-14">
-        <OurDepartments />
+    <section className="w-full">
+      {/* Hero Section */}
+      <div className="pt-9">
+      <Hero />
       </div>
 
-      {/* Stats Section */}
-      <div className="md:pb-24 pb-12">
+        {/* Stats Section */}
+        <div className="pb-20">
         <StatsSection />
       </div>
 
       {/* How It Works */}
-      <div className="pb-8">
+      <div className="pb-20 lg:w-4/5 w-11/12 mx-auto">
         <HowItWorks />
       </div>
 
-      {/* Upcoming Appointments */}
+      {/* Our Services */}
 
-      {/* Clinic and Specialties */}
-      <div className="pb-16">
-        <ClinicAndSpecialties />
+      {/* Our Departments */}
+      <div className="md:pb-20 pb-14 lg:w-4/5 w-11/12 mx-auto">
+        <OurDepartments />
       </div>
 
+
+      {/* Upcoming Appointments */}
+      {/* About us */}
+      <div className="py-8 lg:w-4/5 w-11/12 mx-auto">
+        <AboutUs />
+      </div>
+
+      {/* Clinic and Specialties
+      <div className="pb-16 lg:w-4/5 w-11/12 mx-auto">
+        <ClinicAndSpecialties />
+      </div> */}
+
       {/* Our Medical Experts */}
-      <div className="pb-16">
+      <div className="pb-5 lg:w-4/5 w-11/12 mx-auto">
         <Doctors />
       </div>
 
-      {/* Clinic and Specialties */}
-      <div className="pb-16">
+      {/* Clinic and Specialties
+      <div className="pb-16 lg:w-4/5 w-11/12 mx-auto">
         <ClinicAndSpecialties />
-      </div>
+      </div> */}
 
       {/* Patient Reviews */}
       {/* <PatientReviews /> */}
