18a9b1097d559fd17be6c41e4c9d37a916a739e5
ssmahim01 <mahimzayn666@gmail.com>
2025-03-14 21:30:03 +0600

    [Updated] - html file of index and OurService component

--- src/components/Home/OurService.jsx
+++ src/components/Home/OurService.jsx
@@ -11,14 +11,14 @@ import {
 
 const OurService = () => {
     const services = [
-        { id: 1, name: "Pharmacology", icon: <FaBriefcaseMedical /> },
-        { id: 2, name: "Orthopedic", icon: <FaMicroscope /> },
-        { id: 3, name: "Hematology", icon: <FaHeart /> },
-        { id: 4, name: "Plastic Surgery", icon: <FaUserMd /> },
-        { id: 5, name: "Neurology", icon: <FaBrain /> },
-        { id: 6, name: "Ophthalmology", icon: <FaEye /> },
-        { id: 7, name: "Dental Care", icon: <FaTooth /> },
-        { id: 8, name: "Cardiology", icon: <FaNotesMedical /> },
+        { id: 1, name: "Pharmacology", icon: <FaBriefcaseMedical />, tips:"Advancing drug research and development for better treatments" },
+        { id: 2, name: "Orthopedic", icon: <FaMicroscope />, tips:"Expert care for bone, joint, and muscle health" },
+        { id: 3, name: "Hematology", icon: <FaHeart /> ,tips:"Comprehensive blood disorder diagnosis and treatment" },
+        { id: 4, name: "Plastic Surgery", icon: <FaUserMd />, tips:"Enhancing aesthetics and reconstructive care" },
+        { id: 5, name: "Neurology", icon: <FaBrain /> ,tips:"Cutting-edge solutions for brain and nervous system disorders" },
+        { id: 6, name: "Ophthalmology", icon: <FaEye /> , tips:"Innovative eye care for vision health"},
+        { id: 7, name: "Dental Care", icon: <FaTooth /> ,tips:"Ensuring bright smiles with top-notch dental treatments" },
+        { id: 8, name: "Cardiology", icon: <FaNotesMedical /> ,tips:"Heart health solutions from diagnosis to treatment" },
     ];
 
     return (
@@ -57,7 +57,7 @@ const OurService = () => {
                                 <div className="text-3xl text-[#0E82FD] group-hover:text-white transition">{service.icon}</div>
                                 <h3 className="text-xl font-bold mt-4 group-hover:text-white transition">{service.name}</h3>
                                 <p className="text-sm  opacity-75 mt-2 font-semibold group-hover:text-white transition">
-                                    Medical competitor research startup to financial
+                                   {service.tips}
                                 </p>
                             </div>
 
