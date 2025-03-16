a82aec23b4f9bfd107aec5658baf6d8b889ba74f
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-03-16 21:33:14 +0600

    register page password input added

--- src/authentication/Register.jsx
+++ src/authentication/Register.jsx
@@ -2,10 +2,11 @@ import logo from "@/assets/logo.jpg";
 import { Button } from "@/components/ui/button";
 import { IoIosLogIn } from "react-icons/io";
 import { RiAccountCircleLine, RiLockPasswordLine } from "react-icons/ri";
-import { MdDelete, MdOutlineMail } from "react-icons/md";
+import { MdDelete, MdDone, MdOutlineMail } from "react-icons/md";
 import { Link } from "react-router";
 import { useState } from "react";
 import { FaFileUpload } from "react-icons/fa";
+import { IoEyeOffOutline, IoEyeOutline } from "react-icons/io5";
 
 const Register = () => {
   // states for name, email
@@ -15,10 +16,69 @@ const Register = () => {
   const [image, setImage] = useState("");
   const [preview, setPreview] = useState("");
   // states for password
-  const [password, setPassword] = useState("");
+  const [isEyeOpen, setIsEyeOpen] = useState(false);
+  const [signal, setSignal] = useState({
+    lowercase: false,
+    uppercase: false,
+    number: false,
+    symbol: false,
+    length: false,
+    strong: false,
+  });
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [StrongPassword, setStrongPassword] = useState("");
   // states for loading & error
   const [loading, setLoading] = useState(false);
   const [isError, setIsError] = useState("");
+
+  // Password Validation Functionality
+  const handlePasswordChange = (e) => {
+    const password = e.target.value;
+    setStrongPassword(password);
+
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumber = /[0-9]/.test(password);
+    const hasSymbol = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+    setSignal({
+      lowercase: hasLowerCase,
+      uppercase: hasUpperCase,
+      number: hasNumber,
+      symbol: hasSymbol,
+      length: password.length >= 8,
+      strong:
+        hasUpperCase &&
+        hasLowerCase &&
+        hasNumber &&
+        hasSymbol &&
+        password.length >= 8,
+    });
+  };
+
+  // Password Hinst
+  const hintList = [
+    {
+      text: "Minimum number of characters is 8.",
+      type: "length",
+    },
+    {
+      text: "Should contain uppercase.",
+      type: "uppercase",
+    },
+    {
+      text: "Should contain lowercase.",
+      type: "lowercase",
+    },
+    {
+      text: "Should contain numbers.",
+      type: "number",
+    },
+    {
+      text: "Should contain special characters.",
+      type: "symbol",
+    },
+  ];
 
   // Image Upload Functionality
   const handleUploadImage = () => {
@@ -33,6 +93,12 @@ const Register = () => {
       setPreview(URL.createObjectURL(file));
     }
   };
+
+  // console.log({
+  //   name,
+  //   email,
+  //   image
+  // })
 
   return (
     <div className="w-full min-h-screen flex items-center justify-center bg-base-200 px-4 py-12">
@@ -71,6 +137,8 @@ const Register = () => {
                 type="text"
                 name="text"
                 id="text"
+                required
+                onChange={(e) => setName(e.target.value)}
                 placeholder="Enter Your Name"
                 className="peer border-border border rounded-md outline-none pl-11 pr-5 py-3 w-full focus:ring ring-gray-300 transition-colors duration-300"
               />
@@ -87,7 +155,7 @@ const Register = () => {
             />
             {preview === "" ? (
               <div
-                className="w-full md:w-[100%] flex items-center justify-center flex-col gap-4 border-border border rounded-md py-6 cursor-pointer"
+                className="w-full md:w-[100%] flex items-center justify-center flex-col gap-4 border-border border rounded-md py-4 cursor-pointer"
                 onClick={handleUploadImage}
               >
                 <FaFileUpload className="text-[2rem] text-[#777777]" />
@@ -122,7 +190,6 @@ const Register = () => {
               </div>
             )}
           </div>
-
           {/* Email input */}
           <div>
             {/* Label */}
@@ -136,6 +203,8 @@ const Register = () => {
                 type="email"
                 name="email"
                 id="email"
+                required
+                onChange={(e) => setEmail(e.target.value)}
                 placeholder="Email address"
                 className="peer border-border border rounded-md outline-none pl-11 pr-5 py-3 w-full focus:ring ring-gray-300 transition-colors duration-300"
               />
@@ -153,19 +222,70 @@ const Register = () => {
             </label>
             {/* Input with icon */}
             <div className="w-full mt-2 relative">
-              <RiLockPasswordLine className=" absolute top-3.5 left-3 text-[1.5rem] text-[#777777]" />
+              <RiLockPasswordLine className="absolute top-3.5 left-3 text-[1.5rem] text-[#777777]" />
               <input
-                type="password"
+                type={isEyeOpen ? "text" : "password"}
                 name="password"
                 id="password"
+                onChange={handlePasswordChange}
+                onFocus={() => setIsDropdownOpen(true)}
+                onBlur={() => setIsDropdownOpen(false)}
                 placeholder="Password"
-                className="peer border-border border rounded-md outline-none pl-11 pr-5 py-3 w-full focus:ring ring-gray-300 transition-colors duration-300"
+                className="peer border-border border rounded-md outline-none pl-11 pr-12 py-3 w-full focus:ring ring-gray-300 transition-colors duration-300"
               />
+
+              {isEyeOpen ? (
+                <IoEyeOutline
+                  className="absolute top-3.5 right-3 text-[1.5rem] text-[#777777] cursor-pointer"
+                  onClick={() => setIsEyeOpen(false)}
+                />
+              ) : (
+                <IoEyeOffOutline
+                  className="absolute top-3.5 right-3 text-[1.5rem] text-[#777777] cursor-pointer"
+                  onClick={() => setIsEyeOpen(true)}
+                />
+              )}
+
+              <div
+                className={`${
+                  isDropdownOpen
+                    ? "opacity-100 translate-y-0 z-30"
+                    : "opacity-0 translate-y-[-10px] z-[-1]"
+                } bg-white boxShadow rounded-md py-3 px-4 absolute top-[60px] left-0 w-full transition-all duration-300`}
+              >
+                <h3 className="text-gray-900 font-[500] text-[1rem]">
+                  Your password must contain:
+                </h3>
+
+                <div className="w-full mt-2 flex-col flex gap-[6px]">
+                  {hintList?.map((hint, index) => (
+                    <div
+                      key={index}
+                      className={`${
+                        signal[hint.type]
+                          ? "text-green-500"
+                          : "dark:text-slate-400 text-gray-500"
+                      } text-[0.8rem] flex items-center gap-[8px]`}
+                    >
+                      {signal[hint.type] ? (
+                        <MdDone className="text-[1rem]" />
+                      ) : (
+                        <RxCross1 />
+                      )}
+                      {hint.text}
+                    </div>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
 
           {/* Register Button */}
-          <Button disabled={loading} type="submit" className="mt-1">
+          <Button
+            disabled={loading}
+            type="submit"
+            className="mt-1 bg-black cursor-pointer"
+          >
             {loading ? "Registering..." : "Register"}
           </Button>
         </form>
