d8d10f48c5a1536661aad505b218e0e70303ba5f
sayed725 <ssayed72533@gmail.com>
2025-03-19 10:21:25 +0600

    complete about us details page

--- src/pages/AboutUs/DetailsAboutUs.jsx
+++ src/pages/AboutUs/DetailsAboutUs.jsx
@@ -1,11 +1,13 @@
 import HealthCare from './HeathCare';
 import MoreAboutUs from './MoreAboutUs';
 
+
 const DetailsAboutUs = () => {
     return (
         <div className=' py-16 md:py-20 mx-auto w-11/12 lg:w-10/12 max-w-screen-2xl'>
            <MoreAboutUs />
            <HealthCare />
+          
         </div>
     );
 };
