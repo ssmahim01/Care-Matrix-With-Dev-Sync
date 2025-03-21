e2aa3f4bd672b84d89f6ff13c835c40788e11273
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-03-21 19:28:20 +0600

    Banner page get api issue fixed

--- src/components/Pharmacy/BannerPharma.jsx
+++ src/components/Pharmacy/BannerPharma.jsx
@@ -1,70 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import { toast } from 'sonner';
-import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import sliderbg from "@/assets/slider/slide-bg.jpg";
-import { Swiper, SwiperSlide } from "swiper/react";
+import Slide from "@/components/Pharmacy/Slide";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import Slide from '@/components/Pharmacy/Slide';
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
-import useAxiosSecure from '@/hooks/useAxiosSecure';
-
-
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const BannerPharma = () => {
-    // const [banners, setBanners] = useState([])
-    // const [loading, setLoading] = useState(false)
-    // useEffect(() => {
-    //     setLoading(true)
-    //     fetch('/banners.json')
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             setBanners(data)
-    //             setLoading(false)
-    //         }).catch(error => toast.error(error))
-    // }, [])
-    // const filteredBanners = banners?.filter(banner => banner.status === 'active')
-    // console.log(filteredBanners);
-    const axiosSecure = useAxiosSecure()
-    const { data: banners = [], isLoading } = useQuery({
-        queryKey: ["pharmacy-banners"],
-        queryFn: async () => {
-            const { data } = await axiosSecure.get("/banners");
-            const filtered = data.filter((item) => item.status === "active");
-            return filtered;
-        },
-    });
-    console.log(banners);
+  const { data: banners = [], isLoading } = useQuery({
+    queryKey: ["pharmacy-banners"],
+    queryFn: async () => {
+      const { data } = await axios(`${import.meta.env.VITE_API_URL}/banners`);
+      return data;
+    },
+  });
 
-    return (
-        <div
-            className="bg-no-repeat bg-cover bg-right-top py-12"
-            style={{ backgroundImage: `url('${sliderbg}')` }}
-        >
-            <Swiper
-                spaceBetween={30}
-                centeredSlides={true}
-                autoplay={{
-                    delay: 2500,
-                    disableOnInteraction: false,
-                }}
-                modules={[Autoplay, Pagination, Navigation]}
-                className="mySwiper w-11/12 lg:w-10/12 mx-auto "
-            >
-                {banners?.map((medicine) => (
-                    <SwiperSlide key={medicine._id}>
-                        <Slide
-                            image={medicine.image}
-                            title={medicine.medicineName}
-                            subtitle={medicine.description}
-                        />
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        </div>
-    );
+  return (
+    <div
+      className="bg-no-repeat bg-cover bg-right-top py-12"
+      style={{ backgroundImage: `url('${sliderbg}')` }}
+    >
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper w-11/12 lg:w-10/12 mx-auto "
+      >
+        {banners?.map((medicine) => (
+          <SwiperSlide key={medicine._id}>
+            <Slide
+              image={medicine.image}
+              title={medicine.medicineName}
+              subtitle={medicine.description}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 };
 
 export default BannerPharma;
--- src/pages/OurPharmacy/OurPharmacy.jsx
+++ src/pages/OurPharmacy/OurPharmacy.jsx
@@ -1,22 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import { toast } from 'sonner';
-import { Autoplay, Navigation, Pagination } from "swiper/modules";
-import sliderbg from "@/assets/slider/slide-bg.jpg";
-import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import Slide from '@/components/Pharmacy/Slide';
-import BannerPharma from '@/components/Pharmacy/BannerPharma';
-
+import BannerPharma from "@/components/Pharmacy/BannerPharma";
 
 const OurPharmacy = () => {
-
-    return (
-        <div>
-            <BannerPharma></BannerPharma>
-        </div>
-    );
+  return (
+    <div>
+      <BannerPharma />
+    </div>
+  );
 };
 
 export default OurPharmacy;
