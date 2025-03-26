20d75bcfb3b71759a62c1e9428cb29262b1b1d19
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-03-26 16:30:27 +0600

    Cart json updated

--- src/pages/OurPharmacy/Medicines.jsx
+++ src/pages/OurPharmacy/Medicines.jsx
@@ -6,100 +6,104 @@ import { FaCartPlus } from "react-icons/fa";
 import { Link } from "react-router";
 
 const Medicines = ({ medicines, isLoading }) => {
-  const axiosPublic = useAxiosPublic()
+  const axiosPublic = useAxiosPublic();
 
-  const user = useAuthUser()
-  const [cart, cartLoading, refetch] = useCart()
-  // console.log(user);
-  // add to cart
+  const user = useAuthUser();
+  const [cart, cartLoading, refetch] = useCart();
+
+  // Function For AddToCart
   const handleAddToCart = async (e, medicine) => {
-    e.preventDefault()
-    e.stopPropagation()
+    e.preventDefault();
+    e.stopPropagation();
+
     if (!user) {
-      return toast.error("You must login before add to cart")
+      return toast.error("You must login before add to cart");
     }
+
     const cartItem = {
-      medicineId: medicine._id,
       customer: {
         customerName: user.displayName,
-        customerEmail: user.email
+        customerEmail: user.email,
       },
+      medicineId: medicine._id,
       medicineName: medicine.brandName,
       price: medicine.price.discountedAmount,
       image: medicine.imageURL,
+      strength: medicine.strength,
       quantity: 1,
-      strength: medicine.strength
-    }
-    await toast.promise(axiosPublic.post('/carts', cartItem), {
+    };
+
+    await toast.promise(axiosPublic.post("/carts", cartItem), {
       loading: "Adding to cart...",
       success: <b>Successfully Added To Cart</b>,
       error: (error) => {
-        const errorMessage = error.response?.data?.error || error.message || "Unable to Add";
+        const errorMessage =
+          error.response?.data?.error || error.message || "Unable to Add";
         return <b>{errorMessage}</b>;
-      }
-    })
-    refetch()
-    // console.log(medicine);
+      },
+    });
+    refetch();
   };
 
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-4">
       {isLoading
         ? // Skeleton Loader
-        Array.from({ length: 8 }).map((_, index) => (
-          <div key={index} className="border rounded p-4 animate-pulse">
-            {/* Image Skeleton */}
-            <div className="h-[150px] w-full bg-base-300"></div>
-            {/* Text Skeleton */}
-            <div className="mt-4 space-y-2">
-              <div className="h-6 bg-base-300 rounded w-4/5"></div>
-              <div className="h-5 bg-base-300 rounded w-2/3"></div>
-              <div className="h-7 bg-base-300 rounded mt-4 w-2/5"></div>
+          Array.from({ length: 8 }).map((_, index) => (
+            <div key={index} className="border rounded p-4 animate-pulse">
+              {/* Image Skeleton */}
+              <div className="h-[150px] w-full bg-base-300"></div>
+              {/* Text Skeleton */}
+              <div className="mt-4 space-y-2">
+                <div className="h-6 bg-base-300 rounded w-4/5"></div>
+                <div className="h-5 bg-base-300 rounded w-2/3"></div>
+                <div className="h-7 bg-base-300 rounded mt-4 w-2/5"></div>
+              </div>
             </div>
-          </div>
-        ))
+          ))
         : // Medicines List
-        medicines.map((medicine, index) => (
-          <Link key={index} to={`/medicine/${medicine._id}`}>
-            <div className="border rounded p-4 hover:shadow-xl duration-300 group">
-              {/* Image */}
-              <div className="overflow-hidden">
-                <img
-                  src={medicine?.imageURL}
-                  alt={medicine?.brandName}
-                  className="h-[150px] w-full transform group-hover:scale-110 transition-transform duration-300 object-cover"
-                />
-              </div>
-              {/* Text content */}
-              <div className="mt-4">
-                <h1 className="text-2xl font-bold">
-                  {medicine?.brandName}{" "}
-                  <span className="opacity-70 text-[14px] font-semibold">
-                    {medicine?.strength}
-                  </span>
-                </h1>
-                <h3 className="text-xl font-semibold">
-                  <span className="text-4xl">৳</span>
-                  {medicine?.price?.amount} -{" "}
-                  <span className="opacity-70 text-[16px] font-medium">
-                    <span className="text-4xl">৳</span>
-                    <span className="line-through">
-                      {medicine?.price?.discountedAmount}
+          medicines.map((medicine, index) => (
+            <Link key={index} to={`/medicine/${medicine._id}`}>
+              <div className="border rounded p-4 hover:shadow-xl duration-300 group">
+                {/* Image */}
+                <div className="overflow-hidden">
+                  <img
+                    src={medicine?.imageURL}
+                    alt={medicine?.brandName}
+                    className="h-[150px] w-full transform group-hover:scale-110 transition-transform duration-300 object-cover"
+                  />
+                </div>
+                {/* Text content */}
+                <div className="mt-4">
+                  <h1 className="text-2xl font-bold">
+                    {medicine?.brandName}{" "}
+                    <span className="opacity-70 text-[14px] font-semibold">
+                      {medicine?.strength}
                     </span>
-                  </span>
-                </h3>
-                <button
-                  onClick={(e) => handleAddToCart(e, medicine)}
-                  className="btn btn-sm border-none hover:bg-[#0e6efd] text-[1rem] duration-500 bg-[#0E82FD] text-white mt-4 relative group flex items-center gap-2">
-                  <span className="relative after:content-[''] after:absolute after:left-0 after:bottom-1 after:w-0 after:h-[0.1px] after:bg-white after:transition-all after:duration-300 group-hover:after:w-full">
-                    Add
-                  </span>{" "}
-                  <FaCartPlus size={20} />
-                </button>
+                  </h1>
+                  <h3 className="text-xl font-semibold">
+                    <span className="text-4xl">৳</span>
+                    {medicine?.price?.amount} -{" "}
+                    <span className="opacity-70 text-[16px] font-medium">
+                      <span className="text-4xl">৳</span>
+                      <span className="line-through">
+                        {medicine?.price?.discountedAmount}
+                      </span>
+                    </span>
+                  </h3>
+                  <button
+                    onClick={(e) => handleAddToCart(e, medicine)}
+                    className="btn btn-sm border-none hover:bg-[#0e6efd] text-[1rem] duration-500 bg-[#0E82FD] text-white mt-4 relative group flex items-center gap-2"
+                  >
+                    <span className="relative after:content-[''] after:absolute after:left-0 after:bottom-1 after:w-0 after:h-[0.1px] after:bg-white after:transition-all after:duration-300 group-hover:after:w-full">
+                      Add
+                    </span>{" "}
+                    <FaCartPlus size={20} />
+                  </button>
+                </div>
               </div>
-            </div>
-          </Link>
-        ))}
+            </Link>
+          ))}
     </div>
   );
 };
