21ded96fbc1a3318995aea81c3c90509423a3cc1
Arrafi2644 <raffian2644@gmail.com>
2025-04-08 02:00:52 +0600

    Update favorite doctors page

--- src/pages/DashboardPages/User/MyFavoriteDoctors/MyFavoriteDoctors.jsx
+++ src/pages/DashboardPages/User/MyFavoriteDoctors/MyFavoriteDoctors.jsx
@@ -15,7 +15,7 @@ const MyFavoriteDoctors = () => {
     const { user } = useSelector((state) => state.auth);
     const navigate = useNavigate()
 
-    // console.log(favoriteDoctors);
+    console.log(favoriteDoctors);
     const handleRemove = (_id) => {
         // console.log("REmove ", _id);
 
@@ -42,13 +42,14 @@ const MyFavoriteDoctors = () => {
                             <th>Doctor</th>
                             <th>Rating</th>
                             <th>Experience</th>
+                            <th>Consultation Fee</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody className='font-medium'>
 
                         {
-                            favoriteDoctors?.map((doctor, index) => <tr className='hover:bg-gray-50 border border-gray-300' key={doctor._id}>
+                            favoriteDoctors?.map((doctor, index) => <tr className='hover:bg-gray-50 border border-gray-200' key={doctor._id}>
                                 <th>{index + 1}</th>
                                 <td className='flex gap-2'>
                                     <img className='w-10 h-10 object-cover rounded-lg' src={doctor.doctorInfo.image} alt="" />
@@ -58,20 +59,23 @@ const MyFavoriteDoctors = () => {
                                     </div>
                                 </td>
                                 <td>
-                                    <div className='flex items-center gap-2'><span className='text-yellow-500'><FaStar></FaStar></span> {doctor.doctorInfo.rating}</div>
-                                    </td>
-                                <td > <span className='bg-blue-200 p-1 rounded-md'>{doctor.doctorInfo.experience} </span></td>
+                                    <div className='flex items-center gap-1'><span className='text-yellow-500'><FaStar></FaStar></span> {doctor.doctorInfo.rating}</div>
+                                </td>
+                                <td >
+                                    <span className='bg-blue-100 px-2 py-1 text-sm rounded-md'>{doctor.doctorInfo.experience} </span>
+                                </td>
+                                <td >{doctor.doctorInfo.consultation_fee}</td>
                                 <td>
                                     <div className="dropdown">
                                         <div tabIndex={0} role='button' className="  cursor-pointer p-2 mx-0 rounded  w-fit">
                                             <MoreVertical className="cursor-pointer text-gray-700" />
                                         </div>
                                         <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm right-12 top-0 ">
-                                            <div className='flex items-center gap-4 hover:bg-base-200 cursor-pointer p-2 rounded-sm'> <span><BiDetail size={16} /></span><a>View Details</a></div>                                   
+                                            <div className='flex items-center gap-4 hover:bg-base-200 cursor-pointer p-2 rounded-sm'> <span><BiDetail size={16} /></span><a>View Details</a></div>
 
-                                            <Link  to={`/book-appointment/${doctor.doctorInfo.name}`} state={doctor.doctorInfo._id} className='flex items-center gap-4 hover:bg-base-200 cursor-pointer p-2 rounded-sm'> <span><ClipboardPlus size={16} /></span> <a>Book Appointment</a></Link>
+                                            <Link to={`/book-appointment/${doctor.doctorInfo.name}`} state={doctor.doctorInfo._id} className='flex items-center gap-4 hover:bg-base-200 cursor-pointer p-2 rounded-sm'> <span><ClipboardPlus size={16} /></span> <a>Book Appointment</a></Link>
 
-                                            <div onClick={()=>handleRemove(doctor._id)} className='flex items-center gap-4 hover:bg-base-200 cursor-pointer p-2 rounded-sm'> <span><Trash size={16} /></span> <a>Remove From Favorite</a></div>
+                                            <div onClick={() => handleRemove(doctor._id)} className='flex items-center gap-4 hover:bg-base-200 cursor-pointer p-2 rounded-sm'> <span><Trash size={16} /></span> <a>Remove From Favorite</a></div>
                                         </ul>
                                     </div>
                                 </td>
