df20e5d6b35a58fe12a67f104a83aa780dd96353
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-04-09 18:08:11 +0600

    TopCustomerTable Updated With Image And UID

--- src/pages/DashboardPages/Pharmacist/SalesReport/TopCustomerTable.jsx
+++ src/pages/DashboardPages/Pharmacist/SalesReport/TopCustomerTable.jsx
@@ -1,4 +1,12 @@
 import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import { MoreVertical } from "lucide-react";
+
+import {
   Table,
   TableBody,
   TableCaption,
@@ -16,23 +24,45 @@ const TopCustomerTable = ({ topCustomers }) => {
         <TableRow className={"bg-base-200 hover:bg-base-200"}>
           <TableHead className={"px-6"}>Name</TableHead>
           <TableHead className={"px-6"}>Email</TableHead>
+          <TableHead className={"px-6"}>UID</TableHead>
           <TableHead className={"px-6"}>Phone</TableHead>
           <TableHead className={"px-6"}>Orders</TableHead>
           <TableHead className={"px-6"}>Total Spent</TableHead>
+          <TableHead className={"px-6"}>Actions</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         {topCustomers?.map((customer, idx) => {
           return (
             <TableRow key={idx}>
-              <TableCell className={"px-6"}>{customer?.name}</TableCell>
+              <TableCell className={"px-6 flex items-center gap-4"}>
+                <img
+                  src={customer?.photo}
+                  alt={customer?.name}
+                  className="w-14 h-14 rounded-full object-cover"
+                />{" "}
+                {customer?.name}
+              </TableCell>
               <TableCell className={"px-6"}>{customer?.email}</TableCell>
+              <TableCell className={"px-6"}>{customer?.uid}</TableCell>
               <TableCell className={"px-6"}>{customer?.phone}</TableCell>
               <TableCell className={"px-6"}>
                 {customer?.totalOrders} <sub>(Total)</sub>
               </TableCell>
               <TableCell className={"px-6"}>
                 ৳ {customer?.totalSpent.toFixed(2)}
+              </TableCell>
+              <TableCell className={"px-6"}>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <div className="bg-base-200 p-2 rounded border border-border w-fit cursor-pointer">
+                      <MoreVertical className="text-gray-700" />
+                    </div>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuItem></DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
               </TableCell>
             </TableRow>
           );
