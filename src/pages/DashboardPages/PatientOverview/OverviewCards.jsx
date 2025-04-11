68135ecea20c11162389a8fdcea3cb4560556b5a
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-04-11 16:41:17 +0600

    OverviewCards Updated In PatientOverview Page

--- src/pages/DashboardPages/PatientOverview/OverviewCards.jsx
+++ src/pages/DashboardPages/PatientOverview/OverviewCards.jsx
@@ -1,12 +1,12 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { formatDate } from "date-fns";
 import { Calendar, CreditCard, Heart, Home } from "lucide-react";
 
 const OverviewCards = ({ bedBookings, overviewStats, appointment }) => {
-  console.log(bedBookings);
   return (
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-      <Card className={"border shadow-none border-[#e5e7eb] w-full py-6"}>
+      <Card
+        className={"border shadow-sm border-[#e5e7eb] w-full py-6 rounded-lg"}
+      >
         <CardHeader className="flex flex-row items-center justify-between space-y-0">
           <CardTitle className="text-sm font-medium">
             Upcoming Appointments
@@ -17,12 +17,14 @@ const OverviewCards = ({ bedBookings, overviewStats, appointment }) => {
           <div className="text-2xl font-bold">
             {overviewStats?.upcomingAppointments || 0}
           </div>
-          <p className="text-xs text-muted-foreground">
+          <p className="text-sm text-muted-foreground mt-1">
             Next: {appointment?.date || "None scheduled"}
           </p>
         </CardContent>
       </Card>
-      <Card className={"border shadow-none border-[#e5e7eb] w-full py-6"}>
+      <Card
+        className={"border shadow-sm border-[#e5e7eb] w-full py-6 rounded-lg"}
+      >
         <CardHeader className="flex flex-row items-center justify-between space-y-0">
           <CardTitle className="text-sm font-medium">
             Favorite Doctors
@@ -33,12 +35,14 @@ const OverviewCards = ({ bedBookings, overviewStats, appointment }) => {
           <div className="text-2xl font-bold">
             {overviewStats?.favoriteDoctors || 0}
           </div>
-          <p className="text-xs text-muted-foreground">
+          <p className="text-sm text-muted-foreground mt-1">
             Most recent: {appointment?.doctorName || "None"}
           </p>
         </CardContent>
       </Card>
-      <Card className={"border shadow-none border-[#e5e7eb] w-full py-6"}>
+      <Card
+        className={"border shadow-sm border-[#e5e7eb] w-full py-6 rounded-lg"}
+      >
         <CardHeader className="flex flex-row items-center justify-between space-y-0">
           <CardTitle className="text-sm font-medium">Bed Bookings</CardTitle>
           <Home className="h-4 w-4 text-muted-foreground" />
@@ -47,22 +51,24 @@ const OverviewCards = ({ bedBookings, overviewStats, appointment }) => {
           <div className="text-2xl font-bold">
             {overviewStats?.bedBookingRequests || 0}
           </div>
-          <p className="text-xs text-muted-foreground">
+          <p className="text-sm text-muted-foreground mt-1">
             {bedBookings?.filter((b) => b.status === "accepted").length || 0}{" "}
             accepted
           </p>
         </CardContent>
       </Card>
-      <Card className={"border shadow-none border-[#e5e7eb] w-full py-6"}>
+      <Card
+        className={"border shadow-sm border-[#e5e7eb] w-full py-6 rounded-lg"}
+      >
         <CardHeader className="flex flex-row items-center justify-between space-y-0">
           <CardTitle className="text-sm font-medium">Total Spent</CardTitle>
           <CreditCard className="h-4 w-4 text-muted-foreground" />
         </CardHeader>
         <CardContent className="-mt-3">
-          <div className="text-2xl font-bold">
-            {/* {formatCurrency(overviewStats?.totalSpent || 0)} */}
+          <div className="text-2xl font-extrabold">
+            ৳ {overviewStats?.totalSpent.toFixed(2) || 0}
           </div>
-          <p className="text-xs text-muted-foreground">
+          <p className="text-sm text-muted-foreground mt-1">
             Across {overviewStats?.totalOrders || 0} orders
           </p>
         </CardContent>
