d55a2b7d438b9fd4c241a431ab5637e64e7611ef
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-04-17 00:39:50 +0600

    Sales Report tab content responsive issue fixed

--- src/pages/DashboardPages/Pharmacist/SalesReport/SalesReport.jsx
+++ src/pages/DashboardPages/Pharmacist/SalesReport/SalesReport.jsx
@@ -211,7 +211,7 @@ export default function SalesReport() {
               </CardHeader>
               <CardContent>
                 {/* Revenue Cards */}
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
+                <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-6 mb-8">
                   <RevenueCards
                     avgDailyRevenue={avgDailyRevenue}
                     avgOrderValue={avgOrderValue}
