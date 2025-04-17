5f43cb839af91876131a65fbccd110734cb911f9
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-04-17 14:04:13 +0600

    HeroSearch Doctors Data Added

--- src/pages/Home/Hero/HeroSearch.jsx
+++ src/pages/Home/Hero/HeroSearch.jsx
@@ -1,55 +1,29 @@
 import { Button } from "@/components/ui/button";
+import { useAxiosPublic } from "@/hooks/useAxiosPublic";
+import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { FaRegFile } from "react-icons/fa";
 import { IoIosSearch } from "react-icons/io";
-import { MdOutlineEmail } from "react-icons/md";
-export const peoplesData = [
-  {
-    name: "Emily Johnson",
-    picture:
-      "https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?t=st=1724422719~exp=1724426319~hmac=0ef6c552041a747907df66c476c3e7785bd05b39e09c2f54fa04367876376dfa&w=996",
-    email: "emily.johnson@example.com",
-    emailCount: 12,
-    fileCount: null,
-  },
-  {
-    name: "Michael Brown",
-    picture:
-      "https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?t=st=1724422850~exp=1724426450~hmac=7a97c16dbec9136ae629c05ef06d0c108c5f768493a2cd105ed06e1e059ffca2&w=740",
-    email: "michael.brown@example.com",
-    emailCount: 6,
-    fileCount: 3,
-  },
-  {
-    name: "Sophia Williams",
-    picture:
-      "https://img.freepik.com/free-photo/front-view-smiley-business-man_23-2148479583.jpg?t=st=1724422909~exp=1724426509~hmac=dbbbe6ba54c4c4fd7201076106decb3439f1beb48102a69ce2880283680ee650&w=826",
-    email: "sophia.williams@example.com",
-    emailCount: 24,
-    fileCount: 10,
-  },
-  {
-    name: "James Smith",
-    picture:
-      "https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?t=st=1724422836~exp=1724426436~hmac=170d19af0485e5196848d41f6a7298d320fec8ce665445c9f37ac9ee537bedfc&w=826",
-    email: "james.smith@example.com",
-    emailCount: 5,
-    fileCount: null,
-  },
-  {
-    name: "Olivia Davis",
-    picture:
-      "https://img.freepik.com/free-photo/indoor-studio-shot-attractive-beautiful-pretty-young-woman-wearing-eyeglasses-yellow-sweatshirt-having-long-fair-hair-posing-isolated-pink-wall-people-emotions-concept_176532-6755.jpg?t=st=1724423006~exp=1724426606~hmac=616b0852a84a45ed1da08a2100bb732bc690b807d840e4cf02a0880aff3d7e68&w=996",
-    email: "olivia.davis@example.com",
-    emailCount: 1,
-    fileCount: 12,
-  },
-];
+import { MdMoney, MdOutlineEmail, MdReviews } from "react-icons/md";
+import { Link } from "react-router";
+
 const HeroSearch = () => {
+  const axiosPublic = useAxiosPublic();
   const [keyPressOpen, setKeyPressOpen] = useState(false);
 
+  // Fetch Doctors
+  const { data: doctors, isLoading } = useQuery({
+    queryKey: ["search-doctor-data"],
+    queryFn: async () => {
+      const { data } = await axiosPublic(
+        `/dashboard/administrator/doctors/search`
+      );
+      return data;
+    },
+  });
+
   return (
-    <div className="relative w-full sm:w-[80%] product_search_input z-40">
+    <div className="relative w-full sm:w-full product_search_input z-40">
       <input
         className="px-4 py-2 border border-border bg-white rounded-md w-full pl-[40px] outline-none"
         placeholder="Search..."
@@ -58,9 +32,8 @@ const HeroSearch = () => {
       <IoIosSearch className="absolute top-[9px] left-2 text-[1.5rem] text-[#adadad]" />
 
       <Button
-        variant="outline"
         size="sm"
-        className="absolute top-[4.7px] right-1.5"
+        className="absolute top-[4.7px] right-1.5 bg-blue-600 hover:bg-blue-700 duration-500"
       >
         Search
       </Button>
@@ -71,66 +44,76 @@ const HeroSearch = () => {
           keyPressOpen
             ? "opacity-100 scale-100 pointer-events-auto"
             : "opacity-0 scale-95 pointer-events-none"
-        } fixed sm:absolute top-[110px] left-1/2 -translate-x-1/2 mt-2 w-[95vw] sm:w-[600px] max-w-[95vw] bg-white shadow-2xl border border-gray-200 transition-all duration-300 transform rounded-md z-[9999]`}
+        } fixed sm:absolute top-full mt-2 w-full bg-white shadow-2xl border border-gray-200 transition-all duration-300 transform rounded-md z-50`}
       >
         <div className="p-4">
-          {/* Last Search */}
-          <p className="text-[0.9rem] text-gray-500">Last Search</p>
+          {/* Department */}
+          <p className="text-[0.9rem] text-gray-500">Filter By Department</p>
           <div className="flex items-center gap-[10px] flex-wrap mt-2">
-            {["Programming", "ZenUI Library", "Templates", "Blocks"].map(
-              (term) => (
-                <div
-                  key={term}
-                  className="py-[5px] px-[10px] rounded-full border border-gray-300 text-gray-500 text-[0.7rem] flex items-center gap-[5px] hover:bg-gray-50 cursor-pointer"
-                >
-                  <p>{term}</p>
-                </div>
-              )
-            )}
+            {[
+              "Dental Department",
+              "Neurology Department",
+              "Emergency Department",
+              "Surgery Department",
+            ].map((term) => (
+              <div
+                key={term}
+                className="py-[5px] px-[6px] rounded-full border border-gray-300 text-gray-500 text-[0.7rem] flex items-center gap-[1px] hover:bg-gray-50 cursor-pointer"
+              >
+                <p>{term}</p>
+              </div>
+            ))}
           </div>
 
-          {/* People List */}
+          {/* Doctors List */}
           <div className="border-t border-gray-200 mt-5 pt-[15px]">
-            <p className="text-[0.9rem] text-gray-500">
-              Peoples{" "}
-              <span className="text-[0.8rem] text-gray-400">
-                ({peoplesData.length})
-              </span>
-            </p>
+            <p className="text-[0.9rem] text-gray-500">Doctors</p>
 
             <div className="mt-4 h-[300px] overflow-y-auto">
-              {peoplesData?.map((people, index) => (
+              {doctors?.map((doctor, index) => (
                 <div
                   key={index}
                   className="flex flex-wrap gap-[10px] items-center justify-between w-full hover:bg-gray-100 p-[10px] cursor-pointer rounded-md group"
                 >
                   <div className="flex items-center gap-[15px]">
                     <img
-                      src={people.picture}
+                      src={doctor?.image}
                       alt="avatar"
                       className="w-[50px] h-[50px] rounded-full object-cover"
                     />
                     <div>
-                      <h3 className="text-[1.1rem] font-[500] text-gray-800">
-                        {people.name}
-                      </h3>
                       <p className="text-[0.8rem] break-all text-gray-500">
-                        {people.email}
+                        {doctor?.title}
                       </p>
+                      <h3 className="text-[1.1rem] font-[500] text-gray-800">
+                        {doctor?.name}
+                      </h3>
                     </div>
                   </div>
-
-                  <div className="flex items-center gap-[10px] z-[-1] opacity-0 group-hover:opacity-100 group-hover:z-[1] transition-all duration-300">
-                    <div className="flex items-center gap-[5px] rounded-full bg-white border py-[2px] px-2 border-gray-200 text-[0.8rem] text-gray-500 cursor-pointer">
-                      <MdOutlineEmail className="text-[1rem]" />
-                      {people.emailCount}
-                    </div>
-                    {people.fileCount && (
+                  <div className="flex flex-col gap-2">
+                    <div className="flex items-center gap-[10px] duration-300">
                       <div className="flex items-center gap-[5px] rounded-full bg-white border py-[2px] px-2 border-gray-200 text-[0.8rem] text-gray-500 cursor-pointer">
-                        <FaRegFile className="text-[0.9rem]" />
-                        {people.fileCount}
+                        <MdMoney className="text-[1rem]" />
+                        {doctor?.consultation_fee}
                       </div>
-                    )}
+                      <div className="flex items-center gap-[5px] rounded-full bg-white border py-[2px] px-2 border-gray-200 text-[0.8rem] text-gray-500 cursor-pointer">
+                        <MdReviews className="text-[0.9rem]" />
+                        {doctor?.rating || "1.0"}
+                      </div>
+                    </div>
+                    <div>
+                      <Link to={`/book-appointment/${doctor.name}`}>
+                        <Button
+                          size="xs"
+                          variant={"outline"}
+                          className={
+                            "w-[96%] py-1 px-2 text-gray-800 text-xs cursor-pointer"
+                          }
+                        >
+                          Book Appointment
+                        </Button>
+                      </Link>
+                    </div>
                   </div>
                 </div>
               ))}
