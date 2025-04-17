6c36e04e85c9777ab6afd609dfacb5dcc1d78a60
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-04-17 10:14:17 +0600

    PharmacistRoute Created

--- src/routes/PharmacistRoute.jsx
+++ src/routes/PharmacistRoute.jsx
@@ -1,10 +1,33 @@
+import DashboardLoader from "@/components/Loader/DashboardLoader";
+import { useAuthLoading, useAuthUser } from "@/redux/auth/authActions";
+import { Navigate } from "react-router";
+import useRole from "@/hooks/useRole";
 
 const PharmacistRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const loading = useAuthLoading();
+  const [role, isLoading] = useRole();
+  const user = useAuthUser();
+
+  // Return Loader
+  if (loading || isLoading) return <DashboardLoader />;
+
+  // Return Children
+  if (user && user.email && role === "pharmacist") return children;
+
+  // !role !== "pharmacist" Navigate to dashboard
+  return (
+    <Navigate
+      to={
+        role === "administrator"
+          ? "/dashboard/administrator-overview"
+          : role === "patient"
+          ? "/dashboard/patient-overview"
+          : role === "receptionist"
+          ? "/dashboard/receptionist-overview"
+          : "/"
+      }
+    />
+  );
 };
 
 export default PharmacistRoute;
