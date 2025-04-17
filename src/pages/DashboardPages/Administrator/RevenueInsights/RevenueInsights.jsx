b2ff68ec0f1b5f7f9060068adc30c006164645b5
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-04-17 21:54:39 +0600

    RevenueInsightsHeader Added

--- src/pages/DashboardPages/Administrator/RevenueInsights/RevenueInsights.jsx
+++ src/pages/DashboardPages/Administrator/RevenueInsights/RevenueInsights.jsx
@@ -1,5 +1,13 @@
+import DashboardPagesHeader from "@/shared/Section/DashboardPagesHeader";
+import { CircleDollarSign } from "lucide-react";
+import RevenueInsightsHeader from "./RevenueInsightsHeader";
+
 const RevenueInsights = () => {
-  return <div className="px-7">RevenueInsights</div>;
+  return (
+    <div className="px-7">
+      <RevenueInsightsHeader />
+    </div>
+  );
 };
 
 export default RevenueInsights;
