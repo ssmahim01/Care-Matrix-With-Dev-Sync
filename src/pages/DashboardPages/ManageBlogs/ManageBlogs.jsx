ce50b00db049d801d1ad4e48b0bddf4ea88801a7
sayed725 <ssayed72533@gmail.com>
2025-04-28 15:14:45 +0600

    created manage blogs route

---
+++ src/pages/DashboardPages/ManageBlogs/ManageBlogs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageBlogs = () => {
+    return (
+        <div>
+            add blog cards and input form will placed here
+        </div>
+    );
+};
+
+export default ManageBlogs;
