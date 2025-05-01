37ce276196c2089e4b527248fcdf55458e06bc98
MD Nafizul Iqram <nafizulikram3@gmail.com>
2025-05-01 20:00:36 +0600

    Testimonial file updated

--- src/pages/Home/Patient_Reviews/Testimonials.jsx
+++ src/pages/Home/Patient_Reviews/Testimonials.jsx
@@ -6,16 +6,44 @@ import { Loader2 } from "lucide-react";
 
 export function Testimonials() {
 
-    const { data = [], isLoading } = useQuery({
-        queryKey: ["reviews"],
-        queryFn: async () => {
-          const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/review/all`)
-          return data
-        }
-      }) 
-      // console.log(data)
-      if(isLoading) return <div className="flex items-center justify-center">
-        <Loader2 className="animate-spin duration-200 "/></div>
+  const { data = [], isLoading } = useQuery({
+    queryKey: ["reviews"],
+    queryFn: async () => {
+      const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/review/all`)
+      return data
+    }
+  })
+  // console.log(data)
+  if (isLoading) return <div className="flex items-center justify-center">
+    <Loader2 className="animate-spin duration-200 " /></div>
 
-  return <AnimatedTestimonials testimonials={data} />;
+  return (
+    <section className="testimonials">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            <div id="customers-testimonials" className="owl-carousel">
+              {data.map((name, idx) => (
+                <div className="item" key={idx}>
+                  <div className="shadow-effect">
+                    <img
+                      className="img-circle"
+                      src="http://themes.audemedia.com/html/goodgrowth/images/testimonial3.jpg"
+                      alt={name}
+                    />
+                    <p>
+                      Dramatically maintain clicks-and-mortar solutions without functional
+                      solutions. Completely synergize resource taxing relationships via premier
+                      niche markets. Professionally cultivate.
+                    </p>
+                  </div>
+                  <div className="testimonial-name">{name}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
