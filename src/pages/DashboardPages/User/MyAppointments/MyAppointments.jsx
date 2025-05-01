f78541e0c79bfbc45af4eb900a2a3d187f35d8ce
Arrafi2644 <raffian2644@gmail.com>
2025-05-01 14:52:42 +0600

    Add heading for book appointments page

--- src/pages/DashboardPages/User/MyAppointments/MyAppointments.jsx
+++ src/pages/DashboardPages/User/MyAppointments/MyAppointments.jsx
@@ -373,7 +373,7 @@ const MyAppointments = () => {
     useState(false);
   const [selectedPatient, setSelectedPatient] = useState(null)
   const [selectedPrescription, setSelectedPrescription] = useState(null);
-  console.log(appointments);
+
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowSkeleton(false);
