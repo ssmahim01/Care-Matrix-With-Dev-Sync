402e4a43e29371161d18cdd2e36a1d5b03bcee7c
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-05-03 23:16:38 +0600

    EmptyState Added In ContactMessages

--- src/pages/DashboardPages/Administrator/ContactMessage/ContactMessage.jsx
+++ src/pages/DashboardPages/Administrator/ContactMessage/ContactMessage.jsx
@@ -1,18 +1,21 @@
 import DashboardPagesHeader from "@/shared/Section/DashboardPagesHeader";
 import ContactMessageCard from "./ContactMessageCard";
+import MessageCardSkeleton from "./MessageCardSkeleton";
 import { useQuery } from "@tanstack/react-query";
-import { MailIcon } from "lucide-react";
-import { useState } from "react";
+import { MailIcon, MailWarning } from "lucide-react";
 import { toast } from "sonner";
 import axios from "axios";
-import MessageCardSkeleton from "./MessageCardSkeleton";
+import { Card, CardContent } from "@/components/ui/card";
+import EmptyState from "../../PatientOverview/EmptyState";
 
 const ContactMessage = () => {
   // Fetch ContactMessage
   const {
     data: contacts,
     isLoading,
+    isError,
     refetch,
+    error,
   } = useQuery({
     queryKey: ["contact-message"],
     queryFn: async () => {
@@ -41,6 +44,16 @@ const ContactMessage = () => {
     }
   };
 
+  if (isError) {
+    const errorMessage =
+      error?.message ||
+      "An unexpected error occurred while fetching the doctor data";
+    return toast.error("Error While Fetching Data!", {
+      description: errorMessage,
+      position: "top-right",
+    });
+  }
+
   return (
     <div className="px-5">
       <DashboardPagesHeader
@@ -51,15 +64,33 @@ const ContactMessage = () => {
 
       {/* Messages Cards */}
       <div className="mt-4 space-y-4">
-        {isLoading
-          ? [...Array(3)].map((_, i) => <MessageCardSkeleton key={i} />)
-          : contacts?.map((message, i) => (
-              <ContactMessageCard
-                key={i}
-                message={message}
-                deleteMessage={deleteMessage}
+        {isLoading ? (
+          [...Array(3)].map((_, i) => <MessageCardSkeleton key={i} />)
+        ) : contacts.length === 0 ? (
+          <Card
+            className={
+              "mt-6 border shadow-sm border-[#e5e7eb] w-full py-6 rounded-lg"
+            }
+          >
+            <CardContent>
+              <EmptyState
+                icon={MailWarning}
+                title="No Messages Found"
+                description="You haven’t received any contact messages yet. Once users send you a message, they’ll show up here."
+                actionLabel="Go to Dashboard"
+                actionLink="/dashboard/administrator-overview"
               />
-            ))}
+            </CardContent>
+          </Card>
+        ) : (
+          contacts?.map((message, i) => (
+            <ContactMessageCard
+              key={i}
+              message={message}
+              deleteMessage={deleteMessage}
+            />
+          ))
+        )}
       </div>
     </div>
   );
