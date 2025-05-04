70748df2ef2851fbf7038bb271e2fed7e040b8b8
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-05-04 12:36:16 +0600

    Department Card Design Updated

--- src/pages/DashboardPages/ManageBlogs/ShowBlogTable.jsx
+++ src/pages/DashboardPages/ManageBlogs/ShowBlogTable.jsx
@@ -159,7 +159,7 @@ const ShowBlogTable = ({ blogs, isLoading, refetch }) => {
   return (
     <div className="w-full">
       {/* Blog Cards */}
-      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {blogs?.map((blog, i) => (
           <DashboardBlogCard
             key={i}
